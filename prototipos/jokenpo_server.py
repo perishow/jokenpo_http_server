6f44c5502fa5f92acdf66d675d65e8ea7d029f38
perishow <plm@poli.br>
2025-09-26 13:16:39 -0300

    consegui remover a necessidade do uso de threads e fiz o fluxo de funcionamento básico do server: espera dois usuarios conectarem, entrega o forms, coleta as respostas, entrega o html final. falta calcular quem ganhou e criar um html personalizado para cada informando as jogadas e quem ganhou

---
+++ prototipos/jokenpo_server.py
@@ -0,0 +1,81 @@
+import socket
+import time
+
+endereço = '192.168.1.13'
+porta = 8080
+
+reader = open('site/form_namoral.html','r')
+arquivo = reader.read()
+
+reader2 = open('site/obrigado.html','r')
+arquivo2 = reader2.read()
+
+tamanho_arquivo = str(len(arquivo))
+tamanho_arquivo2 = str(len(arquivo2))
+
+msg_resposta = (f'HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\nContent-Length: {tamanho_arquivo}\r\n\r\n{arquivo}')
+
+msg_resposta2 = (f'HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\nContent-Length: {tamanho_arquivo2}\r\n\r\n{arquivo2}')
+
+usuarios_ativos = {} # {key: ip, value: socket 
+jogadas = {} # {key: ip, value: jogada}  
+
+try:
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((endereço,porta))
+		s.listen(5)
+		print(f'ouvindo em {endereço}:{porta}')
+		
+		# loop que espera 2 jogadores entrarem
+		while len(usuarios_ativos.keys()) < 2:
+			new_sock , addr = s.accept()
+			ip, door = addr
+			if ip not in usuarios_ativos:
+				#usuarios_ativos[ip]= threading.Thread(target=receber_forms, args=(ip, new_sock))
+				usuarios_ativos[ip] = new_sock
+				print(f"usuarios ativos: {usuarios_ativos.keys()}")
+				print(f"tamanho usuarios ativos: {len(usuarios_ativos.keys())}")
+		# loop que inicia o jogo
+		print("buffer cheio, iniciando jogo!")
+		
+		# loop que envia o formulario para os jogadores
+		for ip in usuarios_ativos.keys():
+			usuarios_ativos[ip].sendall(msg_resposta.encode()) # envia um get para receber o formulario
+		
+		# loop que recebe as jogadas
+		n_jogadas = 0
+		sockets = []
+		while n_jogadas < 2:
+			new_sock , addr = s.accept()
+			print(f"recebendo dados de {addr}:")
+			data = new_sock.recv(1024)
+			data_ascii = data.decode('ascii')
+			#print(f"mensagem recebida de {ip}:\n{data_ascii}")
+			data_split = data_ascii.split()
+#				if data_split[0] == 'GET': 
+					#data_bits = bytes_para_binarios(data)
+					#print("dados em bits:\n",data_bits)
+					#print("dados decodificados:\n",data_ascii)
+#					new_sock.sendall(str.encode(msg_resposta))
+			if data_split[0] == 'POST':
+				print(f"jogada de {ip} = \n{data_ascii}")
+				ip, _ = addr
+				jogadas[ip] = data_split[-1].split('=')[1]
+				n_jogadas += 1
+				ip, _ = addr
+				usuarios_ativos[ip] = new_sock
+		print(jogadas)
+		
+		#loop que envia o html de obrigado meu deus vamo nessa porra 
+		#
+		# calcula quem ganhou e faz os html direitinho
+		#
+		
+		for ip in usuarios_ativos:
+			usuarios_ativos[ip].sendall(msg_resposta2.encode())
+		
+			
+except Exception as e:
+	print(f"Erro: {e}")
+	reader.close()
+	reader2.close()
